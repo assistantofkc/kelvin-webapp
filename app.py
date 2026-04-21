bd71ef4b2c07dc58641642bbf3801bf7dd399a11
Kelvin <assistantofkc@gmail.com>
2026-04-21 09:52:46 +0800

    v5.71 - restore original news clipper

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 app = Flask(__name__)
 
 # App version
-APP_VERSION = 'v5.69'
+APP_VERSION = 'v5.68'
 
 
 def generate_sentences(vocabularies, max_retries=2):
