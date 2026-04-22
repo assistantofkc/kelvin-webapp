243d614609bc66ffb507e1c2bb2a65fce0ed75b5
Kelvin <assistantofkc@gmail.com>
2026-04-22 10:31:57 +0800

    v6.17 - version bump

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 app = Flask(__name__)
 
 # App version
-APP_VERSION = 'v6.16'
+APP_VERSION = 'v6.17'
 
 
 def generate_sentences(vocabularies, max_retries=2):
