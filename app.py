73471f3db14ee666fc363975515566df6ba09093
Kelvin <assistantofkc@gmail.com>
2026-04-24 07:27:15 +0800

    v6.28 - Bump to v6.28

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'kelvin-webapp-secret-key-change-in-production')
 
 # App version
-APP_VERSION = 'v6.27'
+APP_VERSION = 'v6.28'
 
 
 def generate_sentences(vocabularies, max_retries=2):
